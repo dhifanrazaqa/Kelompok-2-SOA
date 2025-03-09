4aeabbeff0e3b0efdee8a200f516f0b9888733f6
dhifanrazaqa <dhifanofficial@gmail.com>
2025-03-09 23:17:04 +0700

    feat: change db from psql to mysql

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.use("/api", routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
