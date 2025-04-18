88178f386d8c711dac19312b8e2b03da7f595c2c
dhifanrazaqa <dhifanofficial@gmail.com>
2025-04-18 21:41:41 +0700

    feat: add logger middleware

--- src/middlewares/limiter.middleware.js
+++ src/middlewares/limiter.middleware.js
@@ -4,14 +4,14 @@ const slowDown = require('express-slow-down');
 // Rate limiting: max 100 requests per 15 minutes per IP
 const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 10,
+  max: 25,
   message: 'Terlalu banyak permintaan dari IP ini, coba lagi nanti.',
 });
 
 // Throttling: delay 500ms per request setelah 50 request
 const speedLimiter = slowDown({
   windowMs: 15 * 60 * 1000,
-  delayAfter: 5,
+  delayAfter: 15,
   delayMs: () => 500,
 });
 
